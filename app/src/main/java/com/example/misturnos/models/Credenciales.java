0d839dd51c95b68a87877e2f50cfe0b083c5983f
Lucas Calisi <lucas.calisi@mercadolibre.com>
2020-06-09 22:43:52 -0300

    login integrated

---
+++ app/src/main/java/com/example/misturnos/models/Credenciales.java
@@ -0,0 +1,11 @@
+package com.example.misturnos.models;
+
+public class Credenciales {
+    final String user;
+    final String password;
+
+    public Credenciales(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+}
